54cc7500b0b5c6bb1adadcde3238e584dfbcced2
morganvazy <168125519+morganvazy@users.noreply.github.com>
2026-02-05 21:49:06 -0500

    added interactivity, saved to html

--- R/penglings-R.R
+++ R/penglings-R.R
@@ -1,9 +1,28 @@
 #Using Statistical tools for high-throughput
 #data analysis ggplot2 scatter plots quick guide
 #as reference
+#https://ggplot2.tidyverse.org/reference/labs.html for labels
 
-ggplot(penglings, aes(x=flipper_length_mm, y=body_mass_g
+library(ggplot2)
+library(plotly)
+library(htmlwidgets)
+
+static <- ggplot(penglings, aes(x=flipper_length_mm, y=body_mass_g
   )) + geom_point(aes(color=species, size=bill_length_mm), alpha=0.8) +
-  scale_color_manual(values = c("Adelie" = "#FA9702", "Chinstrap"="#B802FA", "Gentoo"="#018A75"))
+  scale_color_manual(values = c("Adelie" = "#FA9702", "Chinstrap"="#B802FA", "Gentoo"="#018A75")) +
+  labs(
+    x = "Flipper length (mm)",
+    y = "Body mass (g)"
+  )
+
+#https://stackoverflow.com/questions/37465285/interactive-scatter-plots-in-r-overlay-hover-summary-tooltip-as-user-supplied-p
+#used for reference on adding hover feature
+interactive <- ggplotly(static)
+
+#turning it into html file to retain interactivity
+#https://stackoverflow.com/questions/37465285/interactive-scatter-plots-in-r-overlay-hover-summary-tooltip-as-user-supplied-p
+htmlwidgets::saveWidget(
+  interactive, "penglings_R_interactive.html",selfcontained=TRUE
+)
 
 ggsave(filename = "img/MVggplot2_plot.png",width=7,height=5,dpi=300)
