41f37966bccc15b997a6d55b2db1fa802e63f187
morganvazy <168125519+morganvazy@users.noreply.github.com>
2026-02-06 09:50:59 -0500

    Add files via upload

--- altair/penglings-altair.py
+++ altair/penglings-altair.py
@@ -6,11 +6,12 @@ df = pd.read_csv("penglings.csv")
 #https://altair-viz.github.io/gallery/scatter_tooltips.html
 #https://altair-viz.github.io/user_guide/customization.html
 #https://altair-viz.github.io/user_guide/generated/channels/altair.Size.html
+#https://altair-viz.github.io/user_guide/generated/core/altair.Axis.html
 #resource for making a scatterplot
 
 chart = alt.Chart(df).mark_circle().encode(
-    alt.X("flipper_length_mm").scale(zero=False).title("Flipper length (mm)"),
-    alt.Y("body_mass_g").scale(zero=False).title("Body mass (g)"),
+    alt.X("flipper_length_mm", axis=alt.Axis(values = [170, 180, 190, 200, 210, 220, 230])).scale(zero=False).title("Flipper length (mm)"),
+    alt.Y("body_mass_g", axis=alt.Axis(values = [3000, 4000, 5000, 6000])).scale(zero=False).title("Body mass (g)"),
     color = alt.Color("species", scale=alt.Scale(
         domain = ["Adelie", "Chinstrap", "Gentoo"],
         range = ["#FA9702","#B802FA","#018A75"]
@@ -20,4 +21,3 @@ chart = alt.Chart(df).mark_circle().encode(
 ).interactive()
 
 chart.save("penglings_altair.html")
-
