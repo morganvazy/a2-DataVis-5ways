a9f61f640a71e90323cbe523641d9a96fed9bb74
morganvazy <168125519+morganvazy@users.noreply.github.com>
2026-02-06 12:38:58 -0500

    fixed comments

--- R/penglings-R.R
+++ R/penglings-R.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(plotly)
 library(htmlwidgets)
 
+#create a static scatterplot with Penglings data
+#create a color scale with hex values
 static <- ggplot(penglings, aes(x=flipper_length_mm, y=body_mass_g
   )) + geom_point(aes(color=species, size=bill_length_mm), alpha=0.8) +
   scale_color_manual(values = c("Adelie" = "#FA9702", "Chinstrap"="#B802FA", "Gentoo"="#018A75")) +
@@ -15,8 +17,8 @@ static <- ggplot(penglings, aes(x=flipper_length_mm, y=body_mass_g
     y = "Body mass (g)"
   )
 
+#make the graph interactive & add a feature where hovering over a point shows its values
 #https://stackoverflow.com/questions/37465285/interactive-scatter-plots-in-r-overlay-hover-summary-tooltip-as-user-supplied-p
-#used for reference on adding hover feature
 interactive <- ggplotly(static)
 
 #turning it into html file to retain interactivity
@@ -25,4 +27,5 @@ htmlwidgets::saveWidget(
   interactive, "penglings_R_interactive.html",selfcontained=TRUE
 )
 
+#save an image of the plot
 ggsave(filename = "img/MVggplot2_plot.png",width=7,height=5,dpi=300)
